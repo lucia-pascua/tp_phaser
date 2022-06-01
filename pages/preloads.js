b6909c0b84d6baac70a7b90c4f7a618a57c8def4
Lucía Pascua <luciapascua912@gmail.com>
2022-05-31 21:23:20 -0300

    fix 2

--- pages/preloads.js
+++ pages/preloads.js
@@ -5,17 +5,17 @@ export class preloads extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('sky', './assets/sky.png');
-        this.load.image('ground', './assets/platform.png');
-        this.load.image('obstacle', './assets/platform_obstacle.png');
+        this.load.image('sky', '../assets/sky.png');
+        this.load.image('ground', '../assets/platform.png');
+        this.load.image('obstacle', '../assets/platform_obstacle.png');
         this.load.image('star', '../assets/star.png');
-        this.load.image('special_star' , './assets/special_star.png');
-        this.load.image('bomb', './assets/bomb.png');
-        this.load.image('logo', './assets/logo.png');
-        this.load.image('logoGameOver', './assets/budyGO.png')
-        this.load.image('background', './assets/background.png');
-        this.load.spritesheet('dude', './assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-        this.load.audio('music', './assets/music.mp3'); 
+        this.load.image('special_star' , '../assets/special_star.png');
+        this.load.image('bomb', '../assets/bomb.png');
+        this.load.image('logo', '../assets/logo.png');
+        this.load.image('logoGameOver', '../assets/budyGO.png')
+        this.load.image('background', '../assets/background.png');
+        this.load.spritesheet('dude', '../assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.audio('music', '../assets/music.mp3'); 
     }
 
     create(){
